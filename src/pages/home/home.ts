0481428e860274888e58a573e16516a027803cb2
dshall <darion100.dm@gmail.com>
2018-06-19 09:54:12 -0400

    home screen updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  scanedItem: any[] = [];
+  options: BarcodeScannerOptions;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+     public navCtrl: NavController,
+     private barcodeScanner: BarcodeScanner) {
 
   }
 
+  scanBarcode() {
+    this.options = {
+      prompt: "Scan Packages",
+      showTorchButton: true
+    }
+    this.barcodeScanner.scan(this.options).then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
 }
